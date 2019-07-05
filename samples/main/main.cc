f4d5024dcb25a1e6a7f93a4f9a02124c378b2cc5
Prathamesh Netake <prathamesh.netake@gmail.com>
2019-07-05 19:29:56 +0530

    openvino is somewhere in between

--- samples/main/main.cc
+++ samples/main/main.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <inference_engine.hpp>
 #include <opencv2/opencv.hpp>
+#include "src/utils/ocv_common.h"
 /**
  * @class PluginVersion
  * @brief A PluginVersion class stores plugin version and initialization status
@@ -30,29 +31,67 @@ inline void printPluginVersion(InferenceEngine::InferenceEnginePluginPtr ptr, st
 
 int main() {
     std::cout << "InferenceEngine: " << InferenceEngine::GetInferenceEngineVersion() << std::endl;
-    InferenceEngine::InferencePlugin plugin = InferenceEngine::PluginDispatcher().getPluginByDevice("CPU");
+    InferenceEngine::InferencePlugin plugin = InferenceEngine::PluginDispatcher().getPluginByDevice("GPU");
     printPluginVersion(plugin, std::cout);
     InferenceEngine::CNNNetReader cnnNet;
-    cnnNet.ReadNetwork("/home/aitoe/open_model_zoo/model_downloader/Retail/object_detection/pedestrian/rmnet_ssd/0013/dldt/person-detection-retail-0013.xml");
+    cnnNet.ReadNetwork("C:/Users/Alex/open_model_zoo/model_downloader/Retail/object_detection/pedestrian/rmnet_ssd/0013/dldt/person-detection-retail-0013.xml");
     cnnNet.getNetwork().setBatchSize(1);
-    cnnNet.ReadWeights("/home/aitoe/open_model_zoo/model_downloader/Retail/object_detection/pedestrian/rmnet_ssd/0013/dldt/person-detection-retail-0013.bin");
+    cnnNet.ReadWeights("C:/Users/Alex/open_model_zoo/model_downloader/Retail/object_detection/pedestrian/rmnet_ssd/0013/dldt/person-detection-retail-0013.bin");
     std::cout << "Model is read successfully" << std::endl;
     InferenceEngine::InputsDataMap inputInfo = cnnNet.getNetwork().getInputsInfo();
     std::cout << inputInfo.begin()->first << std::endl;
+    InferenceEngine::InputInfo::Ptr& input = inputInfo.begin()->second;
+    auto inputName = inputInfo.begin()->first;
+    input->setPrecision(InferenceEngine::Precision::U8);
+    input->getInputData()->setLayout(InferenceEngine::Layout::NCHW);
 
     // output blob info
     InferenceEngine::OutputsDataMap outputInfo = cnnNet.getNetwork().getOutputsInfo();
     auto outName = outputInfo.begin()->first;
     InferenceEngine::DataPtr& output = outputInfo.begin()->second;
-    InferenceEngine::SizeVector size = output->getDims();
-    std::cout << size[2] << size[3] << std::endl;
+    InferenceEngine::SizeVector size = output->getTensorDesc().getDims();
+    std::cout << size[2] << "==>" << size[3] << std::endl;
+    int objectSize = size[3];
     output->setPrecision(InferenceEngine::Precision::FP32);
     output->setLayout(InferenceEngine::Layout::NCHW);
 
     // load CNN net into the plugin
     InferenceEngine::ExecutableNetwork exeNet = plugin.LoadNetwork(cnnNet.getNetwork(), {});
+    InferenceEngine::InferRequest::Ptr infRequest = exeNet.CreateInferRequestPtr();
+    cv::Mat frame = cv::imread("C:/Users/Alex/Pictures/Camera/IMG_20190104_211344.jpg");
 
-    std::cout << "Executable network is intialized into a plug in" << std::endl;
+    //process the blob
+    InferenceEngine::Blob::Ptr a = infRequest->GetBlob(inputName);
+    matU8ToBlob<uint8_t>(frame, a);
+    std::cout << a->getTensorDesc().getDims()[1] << std::endl;
 
+    // start inference
+    infRequest->StartAsync();
+    if (InferenceEngine::StatusCode::OK == infRequest->Wait(InferenceEngine::IInferRequest::WaitMode::RESULT_READY)) {
+        const float *detections = infRequest->GetBlob(outName)->buffer().as<InferenceEngine::PrecisionTrait<InferenceEngine::Precision::FP32>::value_type*>();
+        const size_t height = (size_t) frame.size().height;
+        const size_t width = (size_t) frame.size().width;
+        for(int i = 0; i < 200; i++) {
+            float image_id = detections[i * objectSize + 0];
+            if (image_id < 0) {
+                std::cout << "Only " << i << " proposals found" << std::endl;
+                break;
+            }
+            float confidence = detections[i * objectSize + 2];
+            if (confidence > 0.1) {
+                auto label = static_cast<int>(detections[i * objectSize + 1]);
+                float xmin = detections[i * objectSize + 3] * width;
+                float ymin = detections[i * objectSize + 4] * height;
+                float xmax = detections[i * objectSize + 5] * width;
+                float ymax = detections[i * objectSize + 6] * height;
+                std::cout << "[" << i << "," << label << "] element, prob = " << confidence <<
+                                    "    (" << xmin << "," << ymin << ")-(" << xmax << "," << ymax << ")"
+                                    << ((confidence > 0.5) ? " WILL BE RENDERED!" : "") << std::endl;
+                cv::rectangle(frame, cv::Point2f(xmin, ymin), cv::Point2f(xmax, ymax), cv::Scalar(0, 0, 255));
+            }
+        }
+        cv::imshow("window", frame);
+        cv::waitKey(0);
+    }
     return 0;
 }
---
+++ src/utils/ocv_common.h
@@ -0,0 +1,34 @@
+#include <inference_engine.hpp>
+#include <opencv2/opencv.hpp>
+
+/**
+* @brief Sets image data stored in cv::Mat object to a given Blob object.
+* @param orig_image - given cv::Mat object with an image data.
+* @param blob - Blob object which to be filled by an image data.
+* @param batchIndex - batch index of an image inside of the blob.
+*/
+template <typename T>
+void matU8ToBlob(const cv::Mat& orig_image, InferenceEngine::Blob::Ptr& blob, int batchIndex = 0) {
+    InferenceEngine::SizeVector blobSize = blob->getTensorDesc().getDims();
+    const size_t width = blobSize[3];
+    const size_t height = blobSize[2];
+    const size_t channels = blobSize[1];
+    T* blob_data = blob->buffer().as<T*>();
+
+    cv::Mat resized_image(orig_image);
+    if (static_cast<int>(width) != orig_image.size().width ||
+            static_cast<int>(height) != orig_image.size().height) {
+        cv::resize(orig_image, resized_image, cv::Size(width, height));
+    }
+
+    int batchOffset = batchIndex * width * height * channels;
+
+    for (size_t c = 0; c < channels; c++) {
+        for (size_t  h = 0; h < height; h++) {
+            for (size_t w = 0; w < width; w++) {
+                blob_data[batchOffset + c * width * height + h * width + w] =
+                        resized_image.at<cv::Vec3b>(h, w)[c];
+            }
+        }
+    }
+}
